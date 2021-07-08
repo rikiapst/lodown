11877848b2d82ad2d562975847ad939bdb5290c9
Ubuntu <ubuntu@ip-172-31-31-100.ec2.internal>
2021-07-08 23:53:19 +0000

    update

--- index.js
+++ index.js
@@ -22,3 +22,206 @@ function each(collection, action) {
     }
 }
 module.exports.each = each;
+
+/**
+ * identity: Designed to return the given value unchanged 
+ * 
+ * @param {valuet} : any data type
+ */
+ function identity (value){
+    return value;
+}
+ 
+ 
+ /**
+ * typeOf: Designed to return the given value's data type as a string 
+ * 
+ * @param {value}: Any data type
+ */
+ 
+ function typeOf(value){
+      if(typeof value === "string"){
+        return "string"
+    }else if (Array.isArray(value) === true){
+        return "array"
+    } else if(typeof value === "object" && Array.isArray(value) !== true && value !== null && value instanceof Date !== true){
+        return "object"
+    } else if(typeof value === "number"){
+        return "number"
+    } else if(typeof value === "boolean"){
+        return "boolean"
+    } else if(value === null){
+        return "null"
+    } else if(value instanceof Date === true){
+        return "date"
+    } else if(typeof value === "function"){
+        return "function"
+    } return "undefined"
+    
+}
+ 
+ /**
+ * First: Designed to loop over a collection, Array or Object, and applies the 
+ * action Function to each value in the collection.
+ * 
+ * @param {Array} collection: The collection over which to iterate.
+ * @param {number} number: The Function to be applied to each value in the 
+ * collection
+ */
+ 
+ function first(array, number){
+   if(!Array.isArray(array)){
+       return [];
+   } else if(typeof number !== "number"){
+       return array[0];
+   } else if(number > array.length - 1){
+       return array;
+   }else {
+       return array.splice(0,number)
+   }
+   
+}
+ /**
+ * last: Designed to loop over a collection, Array or Object, and applies the 
+ * action Function to each value in the collection.
+ * 
+ * @param {Array or Object} collection: The collection over which to iterate.
+ * @param {Function} action: The Function to be applied to each value in the 
+ * collection
+ */
+function last(array, number){
+    if(!Array.isArray(array)){
+        return [];
+    } else if(typeof number !== "number"){
+        return array[array.length - 1];
+    }else if(number > array.length - 1){
+        return array;
+    } else{
+        return array.splice(array.length - number, number)
+    } 
+}
+ /**
+ * identity: Designed to loop over a collection, Array or Object, and applies the 
+ * action Function to each value in the collection.
+ * 
+ * @param {Array or Object} collection: The collection over which to iterate.
+ * @param {Function} action: The Function to be applied to each value in the 
+ * collection
+ */
+ 
+ /**
+ * identity: Designed to loop over a collection, Array or Object, and applies the 
+ * action Function to each value in the collection.
+ * 
+ * @param {Array or Object} collection: The collection over which to iterate.
+ * @param {Function} action: The Function to be applied to each value in the 
+ * collection
+ */
+ 
+ /**
+ * identity: Designed to loop over a collection, Array or Object, and applies the 
+ * action Function to each value in the collection.
+ * 
+ * @param {Array or Object} collection: The collection over which to iterate.
+ * @param {Function} action: The Function to be applied to each value in the 
+ * collection
+ */
+ 
+ /**
+ * identity: Designed to loop over a collection, Array or Object, and applies the 
+ * action Function to each value in the collection.
+ * 
+ * @param {Array or Object} collection: The collection over which to iterate.
+ * @param {Function} action: The Function to be applied to each value in the 
+ * collection
+ */
+ 
+ /**
+ * identity: Designed to loop over a collection, Array or Object, and applies the 
+ * action Function to each value in the collection.
+ * 
+ * @param {Array or Object} collection: The collection over which to iterate.
+ * @param {Function} action: The Function to be applied to each value in the 
+ * collection
+ */
+ 
+ /**
+ * identity: Designed to loop over a collection, Array or Object, and applies the 
+ * action Function to each value in the collection.
+ * 
+ * @param {Array or Object} collection: The collection over which to iterate.
+ * @param {Function} action: The Function to be applied to each value in the 
+ * collection
+ */
+ 
+ /**
+ * identity: Designed to loop over a collection, Array or Object, and applies the 
+ * action Function to each value in the collection.
+ * 
+ * @param {Array or Object} collection: The collection over which to iterate.
+ * @param {Function} action: The Function to be applied to each value in the 
+ * collection
+ */
+ 
+ /**
+ * identity: Designed to loop over a collection, Array or Object, and applies the 
+ * action Function to each value in the collection.
+ * 
+ * @param {Array or Object} collection: The collection over which to iterate.
+ * @param {Function} action: The Function to be applied to each value in the 
+ * collection
+ */
+ 
+ /**
+ * identity: Designed to loop over a collection, Array or Object, and applies the 
+ * action Function to each value in the collection.
+ * 
+ * @param {Array or Object} collection: The collection over which to iterate.
+ * @param {Function} action: The Function to be applied to each value in the 
+ * collection
+ */
+ 
+ /**
+ * identity: Designed to loop over a collection, Array or Object, and applies the 
+ * action Function to each value in the collection.
+ * 
+ * @param {Array or Object} collection: The collection over which to iterate.
+ * @param {Function} action: The Function to be applied to each value in the 
+ * collection
+ */
+ 
+ /**
+ * identity: Designed to loop over a collection, Array or Object, and applies the 
+ * action Function to each value in the collection.
+ * 
+ * @param {Array or Object} collection: The collection over which to iterate.
+ * @param {Function} action: The Function to be applied to each value in the 
+ * collection
+ */
+ 
+ /**
+ * identity: Designed to loop over a collection, Array or Object, and applies the 
+ * action Function to each value in the collection.
+ * 
+ * @param {Array or Object} collection: The collection over which to iterate.
+ * @param {Function} action: The Function to be applied to each value in the 
+ * collection
+ */
+ 
+ /**
+ * identity: Designed to loop over a collection, Array or Object, and applies the 
+ * action Function to each value in the collection.
+ * 
+ * @param {Array or Object} collection: The collection over which to iterate.
+ * @param {Function} action: The Function to be applied to each value in the 
+ * collection
+ */
+ 
+ /**
+ * identity: Designed to loop over a collection, Array or Object, and applies the 
+ * action Function to each value in the collection.
+ * 
+ * @param {Array or Object} collection: The collection over which to iterate.
+ * @param {Function} action: The Function to be applied to each value in the 
+ * collection
+ */
